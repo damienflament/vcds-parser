ba1b83ca5b7828ceaa0a653803b170a28aa1972d
Damien Flament <damien.flament@gmx.com>
2024-04-01 21:58:06 +0200

    Document application functions.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const filePathEl = document.getElementById("scan-file-path");
 const fileNameEl = document.getElementById("scan-file-name");
 const contentEl = document.getElementById("scan-content");
 
+/**
+ * Shows the content of the file specified in the {@link filePathEl} within
+ * the {@link contentEl}.
+ */
 function showFileContent() {
     const reader = new FileReader();
 
@@ -18,14 +22,19 @@ function showFileContent() {
     reader.readAsText(file);
 }
 
-function registerServiceWorker() {
+/**
+ * Registers the specified Service Worker.
+ *
+ * @param {string} path the path to the file to register
+ */
+function registerServiceWorker(path) {
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("sw.js")
+        navigator.serviceWorker.register(path)
             .catch(error => console.error("Service worker registration failed:", error));
     } else {
         console.error("Service workers are not supported");
     }
 }
 
-registerServiceWorker();
+registerServiceWorker("sw.js");
 filePathEl.addEventListener("change", showFileContent);
