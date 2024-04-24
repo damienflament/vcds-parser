e4a708258879995832976289b171930a6b8eff66
Damien Flament <damien.flament@gmx.com>
2024-04-24 21:31:03 +0200

    Use promises chain instead of await.

--- public/lib/filesystem.js
+++ public/lib/filesystem.js
@@ -15,30 +15,24 @@
  * @returns {Promise<PermissionState>} the permission for the given file
  */
 export async function requestPermission (file) {
-  const options = { mode: 'read' }
-
-  let permission = await file.queryPermission(options)
-
-  if (permission === 'prompt') {
-    // The permission must be requested. Try it.
-    try {
-      permission = await file.requestPermission(options)
-
-      // If the permission is still "prompt" after we prompted the user, he
+  return file.queryPermission({ mode: 'read' }).then(p => p === 'prompt'
+    // If the permission is 'prompt', it must be requested.
+    ? file.requestPermission({ mode: 'read' })
+      // If the permission is still 'prompt' after we prompted the user, he
       // musted cancel it.
-      // We regard this as a bug and put the value "denied".
-      if (permission === 'prompt') {
-        permission = 'denied'
-      }
-    } catch (e) {
-      if (e instanceof DOMException && e.name === 'SecurityError') {
-        // The permission has been requested without user interaction
-        permission = 'prompt'
-      }
-    }
-  }
-
-  return permission
+      // We regard this as a bug and put the value 'denied'.
+      .then(p => p === 'prompt' ? 'denied' : p)
+      .catch(e => {
+        if (e instanceof DOMException && e.name === 'SecurityError') {
+          // The permission has been requested without user interaction
+          // (automatically on page load)
+          return 'prompt'
+        } else {
+          throw e
+        }
+      })
+    : p
+  )
 }
 
 /**
